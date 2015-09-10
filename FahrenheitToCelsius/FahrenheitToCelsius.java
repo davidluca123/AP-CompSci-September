a809da8afc066d1f937086d757b0fa6329c03aca
davidluca123 <davidluca123@yahoo.com>
2015-09-09 23:56:56 -0700

    FahrenheitToCelcius HW Day 16
    9/9/15

---
+++ FahrenheitToCelsius/FahrenheitToCelsius.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class FahrenheitToCelsius here.
+ * 
+ * @author (David Lu) 
+ * @version (9/9/15)
+ */
+public class FahrenheitToCelsius
+{
+   public static void main(String[] args) {
+        double F= 100.0;
+        double C= (5.0/9.0)*(F-32.0);
+        
+        System.out.println(C);
+}
+}
